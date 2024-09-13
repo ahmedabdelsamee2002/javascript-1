2ea8dfeaba0fb7bd6edf00248fdd72edeafdaa15
ahmedabdelsamee2002 <141630780+ahmedabdelsamee2002@users.noreply.github.com>
2024-09-14 00:33:14 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,33 @@
+/* Example 1
+Output: undefined
+        2
+        1
+
+Example 2
+Output: undefined 3
+        300
+
+Example 3
+Output: 1
+        2
+        3
+        3
+
+Example 4
+Output: 1
+        2
+        1
+        1
+
+Example 5
+Output: 2
+        undefined
+        undefined
+
+Example 6
+Output: 2
+        2
+        undefined
+
+Example 7
+Output: 400 */
